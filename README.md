af9a626780ff534b8defe32628f1cba46aac7515
celicoo <marcelo.franco.alves@gmail.com>
2020-10-06 12:56:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Writtio ESLint Config
---
+++ index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': [
+      'error',
+      {
+        code: 150,
+      },
+    ],
+    semi: [2, 'never'],
+    quotes: [2, 'single'],
+  },
+}
